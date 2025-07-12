878cce3bb5930ef32eb2315e0beb881d0247754e
Drallz <molokoevodia1@gmail.com>
2025-07-12 21:09:55 +0200

    home  page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,16 @@ function App() {
   return (
     <div className="App">
      <Navbar />
+     <main className="main-content">
+     {/* Hero Section */}
+     <section className="hero">
+          <h1 className="hero-title">Welcome to Drallz Kitchen</h1>
+          <p className="hero-subtitle">
+            Discover mouth-watering recipes, cooking tips, and your next favorite dish.
+          </p>
+          <button className="hero-button">Explore Quick Recipes</button>
+        </section>
+      </main>
     </div>
   );
 }
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,8 +1,7 @@
 export default function Sidebar(){
     return(
-        <p>
-            i got
-
-        </p>
+        <div className="sidebar">
+            sidebar
+        </div>
     )
 }
