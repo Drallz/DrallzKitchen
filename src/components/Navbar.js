a54e4e74e0d1a4b91abc1f7de7173d43026ea111
Drallz <molokoevodia1@gmail.com>
2025-07-12 19:58:56 +0200

    Initial commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,28 @@
+import { useState } from "react"
+import Sidebar from "./Sidebar";
+
+export default function Navbar(){
+    const [showSidebar, setShowSidebar] = useState(false)
+
+    return(
+        <>
+        <div className="navbar container">
+            <a href="#!" className="logo">Drallz<span>Kit</span>Chen🍽️</a>
+
+            <div className="nav-links">
+                <a href="#!">Home</a>
+                <a href="#!">Recipes</a>
+                <a href="#!">Settings</a>
+
+            </div>
+            <div onClick={() => setShowSidebar(!showSidebar)} className={showSidebar ? "sidebar-btn" : "sidebar-btn active"}>
+                <div className="bar"></div>
+                <div className="bar"></div>
+                <div className="bar"></div>
+
+            </div>
+        </div>
+        <Sidebar/>
+        </>
+    )
+}
